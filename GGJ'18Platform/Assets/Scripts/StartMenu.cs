c3c72b2e8ea72977d6802ae8edca3920c5a99ccc
bhdurak <bhdurak@gmail.com>
2018-01-27 01:29:49 +0300

    Added Start Menu

---
+++ GGJ'18Platform/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour {
+
+	public void StartGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
